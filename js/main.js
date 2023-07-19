42f7111b68db6ef1b382d0e20ef3a3c129c3a38e
Leonid.Zhukovets <leonid.zhukovets@gmail.com>
2023-07-19 16:14:22 +0300

    Finish sections and pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const tabsItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabsItem.forEach(function (element) {
+    element.addEventListener('click', open);
+})
+
+function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabsItem.forEach(function (item) {
+        item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function (item) {
+        item.classList.remove('tabs__content-item--active');
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
 const swiper = new Swiper(".swiper", {
     effect: "fade",
     pagination: {
